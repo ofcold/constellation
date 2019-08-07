445e624b4af97eb247e2395517f52e1b49f82be2
Bill Li <bill.li@ofcold.com>
2019-08-08 02:33:44 +0800

    wip

--- src/Tests/ConstellationTest.php
+++ src/Tests/ConstellationTest.php
@@ -2,9 +2,21 @@
 
 namespace Ofcold\Constellation\Tests;
 
+use Carbon\Carbon;
 use DateTime;
 use Mockery;
-use Carbon\Carbon;
+use Ofcold\Constellation\Aries;
+use Ofcold\Constellation\Taurus;
+use Ofcold\Constellation\Gemini;
+use Ofcold\Constellation\Cancer;
+use Ofcold\Constellation\Leo;
+use Ofcold\Constellation\Virgo;
+use Ofcold\Constellation\Libra;
+use Ofcold\Constellation\Scorpio;
+use Ofcold\Constellation\Sagittarius;
+use Ofcold\Constellation\Capricorn;
+use Ofcold\Constellation\Aquarius;
+use Ofcold\Constellation\Pisces;
 use PHPUnit\Framework\TestCase;
 use Ofcold\Constellation\AbstractConstellation;
 use Ofcold\Constellation\CalculatorConstellation;
@@ -29,35 +41,35 @@ class ConstellationTest extends TestCase
 
 	public function testMakeFromString()
 	{
-		$this->assertInstanceOf('Ofcold\Constellation\Aries', CalculatorConstellation::make('1992-03-27'));
-		$this->assertInstanceOf('Ofcold\Constellation\Taurus', CalculatorConstellation::make('1992-04-27'));
-		$this->assertInstanceOf('Ofcold\Constellation\Gemini', CalculatorConstellation::make('1992-05-27'));
-		$this->assertInstanceOf('Ofcold\Constellation\Cancer', CalculatorConstellation::make('1992-06-27'));
-		$this->assertInstanceOf('Ofcold\Constellation\Leo', CalculatorConstellation::make('1992-07-27'));
-		$this->assertInstanceOf('Ofcold\Constellation\Virgo', CalculatorConstellation::make('1992-08-27'));
-		$this->assertInstanceOf('Ofcold\Constellation\Libra', CalculatorConstellation::make('1992-09-27'));
-		$this->assertInstanceOf('Ofcold\Constellation\Scorpio', CalculatorConstellation::make('1992-10-27'));
-		$this->assertInstanceOf('Ofcold\Constellation\Sagittarius', CalculatorConstellation::make('1992-11-27'));
-		$this->assertInstanceOf('Ofcold\Constellation\Capricorn', CalculatorConstellation::make('1992-12-27'));
-		$this->assertInstanceOf('Ofcold\Constellation\Capricorn', CalculatorConstellation::make('1992-01-15'));
-		$this->assertInstanceOf('Ofcold\Constellation\Aquarius', CalculatorConstellation::make('1992-01-26'));
-		$this->assertInstanceOf('Ofcold\Constellation\Pisces', CalculatorConstellation::make('1992-02-27'));
+		$this->assertInstanceOf(Aries::class, CalculatorConstellation::make('1992-03-27'));
+		$this->assertInstanceOf(Taurus::class, CalculatorConstellation::make('1992-04-27'));
+		$this->assertInstanceOf(Gemini::class, CalculatorConstellation::make('1992-05-27'));
+		$this->assertInstanceOf(Cancer::class, CalculatorConstellation::make('1992-06-27'));
+		$this->assertInstanceOf(Leo::class, CalculatorConstellation::make('1992-07-27'));
+		$this->assertInstanceOf(Virgo::class, CalculatorConstellation::make('1992-08-27'));
+		$this->assertInstanceOf(Libra::class, CalculatorConstellation::make('1992-09-27'));
+		$this->assertInstanceOf(Scorpio::class, CalculatorConstellation::make('1992-10-27'));
+		$this->assertInstanceOf(Sagittarius::class, CalculatorConstellation::make('1992-11-27'));
+		$this->assertInstanceOf(Capricorn::class, CalculatorConstellation::make('1992-12-27'));
+		$this->assertInstanceOf(Capricorn::class, CalculatorConstellation::make('1992-01-15'));
+		$this->assertInstanceOf(Aquarius::class, CalculatorConstellation::make('1992-01-26'));
+		$this->assertInstanceOf(Pisces::class, CalculatorConstellation::make('1992-02-27'));
 	}
 
 	public function testMakeFromObject()
 	{
-		$this->assertInstanceOf('Ofcold\Constellation\Aries', CalculatorConstellation::make(new DateTime('1992-03-27')));
-		$this->assertInstanceOf('Ofcold\Constellation\Taurus', CalculatorConstellation::make(new DateTime('1992-04-27')));
-		$this->assertInstanceOf('Ofcold\Constellation\Gemini', CalculatorConstellation::make(new DateTime('1992-05-27')));
-		$this->assertInstanceOf('Ofcold\Constellation\Cancer', CalculatorConstellation::make(new DateTime('1992-06-27')));
-		$this->assertInstanceOf('Ofcold\Constellation\Leo', CalculatorConstellation::make(new DateTime('1992-07-27')));
-		$this->assertInstanceOf('Ofcold\Constellation\Virgo', CalculatorConstellation::make(new DateTime('1992-08-27')));
-		$this->assertInstanceOf('Ofcold\Constellation\Libra', CalculatorConstellation::make(new DateTime('1992-09-27')));
-		$this->assertInstanceOf('Ofcold\Constellation\Scorpio', CalculatorConstellation::make(new DateTime('1992-10-27')));
-		$this->assertInstanceOf('Ofcold\Constellation\Sagittarius', CalculatorConstellation::make(new DateTime('1992-11-27')));
-		$this->assertInstanceOf('Ofcold\Constellation\Capricorn', CalculatorConstellation::make(new DateTime('1992-12-27')));
-		$this->assertInstanceOf('Ofcold\Constellation\Capricorn', CalculatorConstellation::make(new DateTime('1992-01-15')));
-		$this->assertInstanceOf('Ofcold\Constellation\Aquarius', CalculatorConstellation::make(new DateTime('1992-01-26')));
-		$this->assertInstanceOf('Ofcold\Constellation\Pisces', CalculatorConstellation::make(new DateTime('1992-02-27')));
+		$this->assertInstanceOf(Aries::class, CalculatorConstellation::make(new DateTime('1992-03-27')));
+		$this->assertInstanceOf(Taurus::class, CalculatorConstellation::make(new DateTime('1992-04-27')));
+		$this->assertInstanceOf(Gemini::class, CalculatorConstellation::make(new DateTime('1992-05-27')));
+		$this->assertInstanceOf(Cancer::class, CalculatorConstellation::make(new DateTime('1992-06-27')));
+		$this->assertInstanceOf(Leo::class, CalculatorConstellation::make(new DateTime('1992-07-27')));
+		$this->assertInstanceOf(Virgo::class, CalculatorConstellation::make(new DateTime('1992-08-27')));
+		$this->assertInstanceOf(Libra::class, CalculatorConstellation::make(new DateTime('1992-09-27')));
+		$this->assertInstanceOf(Scorpio::class, CalculatorConstellation::make(new DateTime('1992-10-27')));
+		$this->assertInstanceOf(Sagittarius::class, CalculatorConstellation::make(new DateTime('1992-11-27')));
+		$this->assertInstanceOf(Capricorn::class, CalculatorConstellation::make(new DateTime('1992-12-27')));
+		$this->assertInstanceOf(Capricorn::class, CalculatorConstellation::make(new DateTime('1992-01-15')));
+		$this->assertInstanceOf(Aquarius::class, CalculatorConstellation::make(new DateTime('1992-01-26')));
+		$this->assertInstanceOf(Pisces::class, CalculatorConstellation::make(new DateTime('1992-02-27')));
 	}
 }
