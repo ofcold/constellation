ba0aa3515b0755ffe80651800a22d26ede6d696e
Bill li <bill.li@ofcold.com>
2021-05-21 11:34:50 +0800

    format

--- src/Capricorn.php
+++ src/Capricorn.php
@@ -45,12 +45,13 @@ class Capricorn extends AbstractConstellation
 	 */
 	public function match(Carbon $date): bool
 	{
-		$start1 = Carbon::create($date->year, $this->start['month'], $this->start['day'], 0, 0, 0);
-		$end1 = Carbon::create($date->year, 12, 31, 0, 0, 0)->addDay();
-
-		$start2 = Carbon::create($date->year, 1, 1, 0, 0, 0);
-		$end2 = Carbon::create($date->year, $this->end['month'], $this->end['day'], 0, 0, 0)->addDay();
-
-		return ($date->between($start1, $end1) || $date->between($start2, $end2));
+		return $date->between(
+				Carbon::create($date->year, $this->start['month'], $this->start['day'], 0, 0, 0),
+				Carbon::create($date->year, 12, 31, 0, 0, 0)->addDay()
+			)
+			 || $date->between(
+			 	Carbon::create($date->year, 1, 1, 0, 0, 0),
+			 	Carbon::create($date->year, $this->end['month'], $this->end['day'], 0, 0, 0)->addDay()
+			);
 	}
 }
